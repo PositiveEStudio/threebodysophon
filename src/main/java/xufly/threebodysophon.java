67216e4a32bf78ece6e599a28683b86eff6a5593
XuFly <mcxufly@qq.com>
2021-07-03 09:37:14 +0800

    更新Mod信息

--- src/main/java/xufly/threebodysophon.java
+++ src/main/java/xufly/threebodysophon.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package xufly;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
@@ -19,12 +19,12 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod {
+@Mod("threebodysophon")
+public class threebodysophon {
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public ExampleMod() {
+    public threebodysophon() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
@@ -51,7 +51,7 @@ public class ExampleMod {
 
     private void enqueueIMC(final InterModEnqueueEvent event) {
         // some example code to dispatch IMC to another mod
-        InterModComms.sendTo("examplemod", "helloworld", () -> {
+        InterModComms.sendTo("threebodysophon", "helloworld", () -> {
             LOGGER.info("Hello world from the MDK");
             return "Hello world";
         });
