82e50a47a68ad8c9870e58734465387da6285271
XuFly <mcxufly@qq.com>
2021-07-03 10:45:20 +0800

    完善Mod信息

--- src/main/java/xufly/threebodysophon/threebodysophon.java
+++ src/main/java/xufly/threebodysophon/threebodysophon.java
@@ -1,4 +1,4 @@
-package xufly;
+package xufly.threebodysophon;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
@@ -18,11 +18,9 @@ import org.apache.logging.log4j.Logger;
 
 import java.util.stream.Collectors;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod("threebodysophon")
 public class threebodysophon
 {
-    // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
     public threebodysophon()
