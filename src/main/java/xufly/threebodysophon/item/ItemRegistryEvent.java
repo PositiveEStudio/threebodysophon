dca8219bf33eda6b6a3202053823266ddb26ddf7
XuFly <mcxufly@qq.com>
2021-07-06 00:06:12 +0800

    .

--- src/main/java/xufly/threebodysophon/item/ItemRegistryEvent.java
+++ src/main/java/xufly/threebodysophon/item/ItemRegistryEvent.java
@@ -1,13 +1,10 @@
 ﻿package xufly.threebodysophon.item;
 
-import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.registries.DeferredRegister;
-
 
 @Mod.EventBusSubscriber
 public class ItemRegistryEvent
 {
-    public static final Item Proton = new ItemProton();
+    //public static final Item Proton = new ItemProton();
 
 }
