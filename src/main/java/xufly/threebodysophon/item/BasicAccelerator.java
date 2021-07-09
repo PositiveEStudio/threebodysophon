e0f8d1fbc8e735b93dd04e0fbfaf75db70acaafa
XuFly <mcxufly@qq.com>
2021-07-10 00:33:29 +0800

    基础加速器

---
+++ src/main/java/xufly/threebodysophon/item/BasicAccelerator.java
@@ -0,0 +1,15 @@
+package xufly.threebodysophon.item;
+
+import net.minecraft.item.Item;
+import xufly.threebodysophon.creative.ModGroup;
+
+/**
+ * @author XuFly
+ */
+public class BasicAccelerator extends Item
+{
+    public BasicAccelerator()
+    {
+        super(new Properties().tab(ModGroup.itemGroup));
+    }
+}
