83334dfd23400d7f08da733679d73d581342cbd4
XuFly <mcxufly@qq.com>
2021-07-10 20:40:52 +0800

    加速组件

---
+++ src/main/java/xufly/threebodysophon/item/ItemAccelerationComponent.java
@@ -0,0 +1,12 @@
+package xufly.threebodysophon.item;
+
+import net.minecraft.item.Item;
+import xufly.threebodysophon.creative.ModGroup;
+
+public class ItemAccelerationComponent extends Item
+{
+    public ItemAccelerationComponent()
+    {
+        super(new Properties().tab(ModGroup.itemGroup));
+    }
+}
