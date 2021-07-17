caa189057eb7f545223999adafd1e931eaef2213
XuFly <mcxufly@qq.com>
2021-07-18 00:29:09 +0800

    Revert "."
    This reverts commit 96c8384a

--- src/main/java/xufly/threebodysophon/item/ItemRegistryHandler.java
+++ src/main/java/xufly/threebodysophon/item/ItemRegistryHandler.java
@@ -1,14 +1,11 @@
 package xufly.threebodysophon.item;
 
-import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import xufly.threebodysophon.ThreebodySophon;
-import xufly.threebodysophon.block.BlockRegistryHandler;
-import xufly.threebodysophon.creative.ModGroup;
 
 /**
  * @author XuFly
@@ -19,11 +16,4 @@ public class ItemRegistryHandler
 {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ThreebodySophon.MODID);
     public static RegistryObject<Item> PROTON = ITEMS.register("proton", ItemProton::new);
-    public static RegistryObject<Item> MATERIAL_SINGULARITY_17 = ITEMS.register("material_singularity_17", ItemMaterialSingularity17::new);
-    public static RegistryObject<Item> BASIC_ACCELERATOR = ITEMS.register("basic_accelerator", ItemMaterialSingularity17::new);
-    public static RegistryObject<Item> ACCELERATION_COMPONENT = ITEMS.register("acceleration_component", ItemAccelerationComponent::new);
-    public static RegistryObject<Item> CONTROL_CORE = ITEMS.register("control_core", ItemControlCore::new);
-    public static RegistryObject<Item> ENGRAVING_COMPONENT = ITEMS.register("engraving_component", ItemEngravingComponent::new);
-    public static RegistryObject<Item> COMPUTING_COMPONENT = ITEMS.register("circuit_computing_component", ItemCircuitComputingComponent::new);
-    public static RegistryObject<Item> QUANTUM_ENGRAVING_MACHINE = ITEMS.register("quantum_engraving_machine", () -> new BlockItem(BlockRegistryHandler.QUANTUM_ENGRAVING_MACHINE.get(), new Item.Properties().tab(ModGroup.itemGroup)));
 }
