40c4330c2c83ba7e8ef14690ed5fd6e6c2c66313
XuFly <mcxufly@qq.com>
2021-07-18 01:14:12 +0800

    进入游戏消息

--- src/main/java/xufly/threebodysophon/event/EventHandler.java
+++ src/main/java/xufly/threebodysophon/event/EventHandler.java
@@ -1,11 +1,10 @@
 package xufly.threebodysophon.event;
 
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -17,15 +16,13 @@ import net.minecraftforge.fml.common.Mod;
 public class EventHandler
 {
     @SubscribeEvent
-    public static void onPlayerJoin(EntityJoinWorldEvent event)
+    public static void onPlayerLoggedIn(PlayerEvent.PlayerLoggedInEvent event)
     {
-        Entity entity = event.getEntity();
-        if (entity instanceof PlayerEntity)
+        //"消灭人类暴政，世界属于三体！"
+        ITextComponent component = new StringTextComponent("\u6d88\u706d\u4eba\u7c7b\u66b4\u653f\uff0c\u4e16\u754c\u5c5e\u4e8e\u4e09\u4f53\uff01");
+        if (event.getPlayer() instanceof ServerPlayerEntity)
         {
-            //String message = "消灭人类暴政，世界属于三体！";
-            String message = "\u6d88\u706d\u4eba\u7c7b\u66b4\u653f\uff0c\u4e16\u754c\u5c5e\u4e8e\u4e09\u4f53\uff01";
-            ITextComponent text = new StringTextComponent(message);
-            entity.sendMessage(text, entity.getUUID());
+            event.getPlayer().sendMessage(component, event.getPlayer().getUUID());
         }
     }
 }
