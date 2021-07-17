96c8384acf7ea9f6a90d55cf79d03d0ab22c86cc
XuFly <mcxufly@qq.com>
2021-07-17 15:19:15 +0800

    .

--- src/main/java/xufly/threebodysophon/item/ItemRegistryHandler.java
+++ src/main/java/xufly/threebodysophon/item/ItemRegistryHandler.java
@@ -16,6 +16,4 @@ public class ItemRegistryHandler
 {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ThreebodySophon.MODID);
     public static RegistryObject<Item> PROTON = ITEMS.register("proton", ItemProton::new);
-    public static RegistryObject<Item> MATERIAL_SINGULARITY_17 = ITEMS.register("material_singularity_17", ItemMaterialSingularity17::new);
-    public static RegistryObject<Item> BASICACCELERATOR = ITEMS.register("basic_accelerator", ItemMaterialSingularity17::new);
 }
