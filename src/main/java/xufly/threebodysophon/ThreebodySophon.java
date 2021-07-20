df95af447f0a333789ad6a2afca5869ca4fb53de
XuFly <mcxufly@qq.com>
2021-07-21 01:05:39 +0800

    控制核心 加速组件 基础加速器 无材质 无模型

--- src/main/java/xufly/threebodysophon/ThreebodySophon.java
+++ src/main/java/xufly/threebodysophon/ThreebodySophon.java
@@ -42,7 +42,7 @@ public class ThreebodySophon
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
-        ItemRegistryHandler.ItemRegister.register(FMLJavaModLoadingContext.get().getModEventBus());
+        ItemRegistryHandler.ITEM_REGISTER.register(FMLJavaModLoadingContext.get().getModEventBus());
     }
 
     private void setup(final FMLCommonSetupEvent event)
