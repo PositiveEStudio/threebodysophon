29a38add3d41e5b665ae9272ac65b8446e022d59
XuFly <mcxufly@qq.com>
2021-07-27 22:42:49 +0800

    低维展开仪及测试材质

--- src/main/java/xufly/threebodysophon/block/BlockLowDimensionalExpansionInstrument.java
+++ src/main/java/xufly/threebodysophon/block/BlockLowDimensionalExpansionInstrument.java
@@ -5,14 +5,14 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.HorizontalBlock;
 import net.minecraft.block.material.Material;
+import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.state.EnumProperty;
+import net.minecraft.state.StateContainer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
 import net.minecraft.world.IBlockReader;
 import net.minecraftforge.common.ToolType;
 import xufly.threebodysophon.tileentity.TileEntityLowDimensionalExpansionInstrument;
-
-import javax.annotation.Nullable;
 
 public class BlockLowDimensionalExpansionInstrument extends Block
 {
@@ -21,6 +21,7 @@ public class BlockLowDimensionalExpansionInstrument extends Block
     public BlockLowDimensionalExpansionInstrument()
     {
         super(AbstractBlock.Properties.create(Material.IRON).harvestTool(ToolType.PICKAXE));
+        this.setDefaultState(this.getStateContainer().getBaseState().with(FACING, Direction.NORTH));
     }
 
     @Override
@@ -29,10 +30,22 @@ public class BlockLowDimensionalExpansionInstrument extends Block
         return true;
     }
 
-    @Nullable
     @Override
     public TileEntity createTileEntity(BlockState state, IBlockReader world)
     {
         return new TileEntityLowDimensionalExpansionInstrument();
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockItemUseContext blockItemUseContext)
+    {
+        return this.getDefaultState().with(FACING, blockItemUseContext.getPlacementHorizontalFacing().getOpposite());
+    }
+
+    @Override
+    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder)
+    {
+        builder.add(FACING);
+        super.fillStateContainer(builder);
     }
 }
