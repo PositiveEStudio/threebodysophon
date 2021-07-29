6d079316eb9754dc634c57c558b1bbf41f50acab
XuFly <mcxufly@qq.com>
2021-07-30 00:39:48 +0800

    Revert "控制器UI小修改"
    This reverts commit 4ef4b2a7

--- src/main/java/xufly/threebodysophon/client/gui/screen/ScreenSophonControllerUsing.java
+++ src/main/java/xufly/threebodysophon/client/gui/screen/ScreenSophonControllerUsing.java
@@ -9,39 +9,40 @@ import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 import xufly.threebodysophon.ThreebodySophon;
 
-public class ScreenSophonControllerUsing extends Screen
+public class SophonControllerScreen extends Screen
 {
-    private static final ResourceLocation TEXTURE = new ResourceLocation(ThreebodySophon.MODID, "textures/gui/sophon_controller_using.png");
-    private final int xSize = 176;
-    private final int ySize = 166;
+    private static final ResourceLocation TEXTURE = new ResourceLocation(ThreebodySophon.MODID, "textures/gui/sophon_controller.png");
+    private int xSize = 176;
+    private int ySize = 166;
     private int guiLeft;
     private int guiTop;
     private boolean usePos;
 
-    protected ScreenSophonControllerUsing()
+    protected SophonControllerScreen()
     {
-        super(new StringTextComponent("screen.sophon_controller_using.title"));
+        super(new StringTextComponent("sophon_controller"));
+        this.guiLeft = (this.width - xSize) / 2;
         this.usePos = true;
     }
 
     @Override
     protected void init()
     {
-        this.guiLeft = (this.width - this.xSize) / 2;
+        this.guiLeft = (this.width - xSize) / 2;
         this.guiTop = (this.height - this.ySize) / 2;
     }
 
     @Override
     public void render(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks)
     {
-        super.render(matrixStack, mouseX, mouseY, partialTicks);
         this.renderBackground(matrixStack);
+        super.render(matrixStack, mouseX, mouseY, partialTicks);
         RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
 
         this.minecraft.getTextureManager().bindTexture(TEXTURE);
         this.blit(matrixStack, this.guiLeft, this.guiTop, 0, 0, this.xSize, this.ySize);
 
-        TranslationTextComponent title = new TranslationTextComponent("screen.sophon_controller_using.title");
+        TranslationTextComponent title = new TranslationTextComponent("screen.sophon_controller.title");
         this.font.drawText(matrixStack, title, (this.width - this.font.getStringWidth(title.getString())) / 2.0F, this.guiTop + 6.0F, 0xFF9999);
     }
 
@@ -61,5 +62,4 @@ public class ScreenSophonControllerUsing extends Screen
         }
         return super.keyPressed(keyCode, scanCode, modifiers);
     }
-
 }
