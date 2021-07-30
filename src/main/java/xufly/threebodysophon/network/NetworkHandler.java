0a9e4ae11035ce95fd754d0d4eafe25c69c0b176
vvvbbbcz <vvvbbbcz@163.com>
2021-07-31 00:20:29 +0800

    功能完善

--- src/main/java/xufly/threebodysophon/network/NetworkHandler.java
+++ src/main/java/xufly/threebodysophon/network/NetworkHandler.java
@@ -4,6 +4,7 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.network.NetworkRegistry;
 import net.minecraftforge.fml.network.simple.SimpleChannel;
 import xufly.threebodysophon.ThreebodySophon;
+import xufly.threebodysophon.network.client.CSophonExpansionPacket;
 import xufly.threebodysophon.network.client.CSophonPositionPacket;
 
 public class NetworkHandler
@@ -12,12 +13,14 @@ public class NetworkHandler
     public static final SimpleChannel SOPHON_POSITION = NetworkRegistry.newSimpleChannel(new ResourceLocation(ThreebodySophon.MODID, "sophon_position"), () -> VERSION, (v) -> v.equals(VERSION), (v) -> v.equals(VERSION));
     public static final SimpleChannel SOPHON_DISPLAY_TEXT = NetworkRegistry.newSimpleChannel(new ResourceLocation(ThreebodySophon.MODID, "sophon_display_text"), () -> VERSION, (v) -> v.equals(VERSION), (v) -> v.equals(VERSION));
     public static final SimpleChannel SOPHON_TEXT_ON_PLAYER_EYE = NetworkRegistry.newSimpleChannel(new ResourceLocation(ThreebodySophon.MODID, "sophon_text_on_player_eye"), () -> VERSION, (v) -> v.equals(VERSION), (v) -> v.equals(VERSION));
+    public static final SimpleChannel SOPHON_EXPANSION = NetworkRegistry.newSimpleChannel(new ResourceLocation(ThreebodySophon.MODID, "sophon_expansion"), () -> VERSION, (v) -> v.equals(VERSION), (v) -> v.equals(VERSION));
 
     private static int id = 0;
 
     public static void register()
     {
         SOPHON_POSITION.messageBuilder(CSophonPositionPacket.class, getId()).encoder(CSophonPositionPacket::encode).decoder(CSophonPositionPacket::new).consumer(CSophonPositionPacket::consumer).add();
+        SOPHON_EXPANSION.messageBuilder(CSophonExpansionPacket.class, getId()).encoder(CSophonExpansionPacket::encode).decoder(CSophonExpansionPacket::new).consumer(CSophonExpansionPacket::consumer).add();
     }
 
     private static int getId()
--- src/main/java/xufly/threebodysophon/network/client/CSophonPositionPacket.java
+++ src/main/java/xufly/threebodysophon/network/client/CSophonPositionPacket.java
@@ -1,20 +1,21 @@
 package xufly.threebodysophon.network.client;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.network.PacketBuffer;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.fml.network.NetworkEvent;
-import xufly.threebodysophon.ThreebodySophon;
 import xufly.threebodysophon.entity.EntitySophon;
 
 import java.util.Objects;
 import java.util.UUID;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 public class CSophonPositionPacket
 {
-    private final int sophonId;
-    private final boolean pos; // true:绝对坐标，false:玩家面前
+    private final UUID sophonId;
+    private final boolean usePos; // true:绝对坐标，false:玩家面前
     private final double posX;
     private final double posY;
     private final double posZ;
@@ -22,28 +23,35 @@ public class CSophonPositionPacket
 
     public CSophonPositionPacket(PacketBuffer buffer)
     {
-        this.sophonId = buffer.readInt();
-        this.pos = buffer.readBoolean();
+        this.sophonId = buffer.readUniqueId();
+        this.usePos = buffer.readBoolean();
         this.posX = buffer.readDouble();
         this.posY = buffer.readDouble();
         this.posZ = buffer.readDouble();
         this.trackingPlayer = buffer.readUniqueId();
     }
 
-    public CSophonPositionPacket(int sophonId, boolean pos, double x, double y, double z, UUID trackingPlayer)
+    public CSophonPositionPacket(UUID sophonId, boolean usePos, double x, double y, double z, UUID trackingPlayer)
     {
         this.sophonId = sophonId;
-        this.pos = pos;
+        this.usePos = usePos;
         this.posX = x;
         this.posY = y;
         this.posZ = z;
-        this.trackingPlayer = trackingPlayer;
+        if (this.usePos) // 防止出现空指针
+        {
+            this.trackingPlayer = UUID.randomUUID();
+        }
+        else
+        {
+            this.trackingPlayer = trackingPlayer;
+        }
     }
 
     public void encode(PacketBuffer buffer)
     {
-        buffer.writeInt(this.sophonId);
-        buffer.writeBoolean(this.pos);
+        buffer.writeUniqueId(this.sophonId);
+        buffer.writeBoolean(this.usePos);
         buffer.writeDouble(this.posX);
         buffer.writeDouble(this.posY);
         buffer.writeDouble(this.posZ);
@@ -55,21 +63,23 @@ public class CSophonPositionPacket
         context.get().enqueueWork(() -> {
             try
             {
+                EntitySophon sophon = null;
                 World world = Objects.requireNonNull(context.get().getSender()).world;
-                if (world.getEntityByID(this.sophonId) instanceof EntitySophon)
+                for (Entity entity : ((ServerWorld) world).getEntities().collect(Collectors.toList()))
                 {
-                    EntitySophon sophon = (EntitySophon) world.getEntityByID(this.sophonId);
-                    if (pos)
+                    if (entity instanceof EntitySophon && entity.getUniqueID().equals(this.sophonId))
                     {
-                        if (world.isAreaLoaded(new BlockPos(this.posX, this.posY, this.posZ), 0))
-                        {
-                            sophon.setPosition(this.posX, this.posY, this.posZ);
-                        }
+                        sophon = (EntitySophon) entity;
                     }
-                    else
-                    {
-                        sophon.setTrackingPlayer(this.trackingPlayer);
-                    }
+                }
+
+                if (usePos)
+                {
+                    sophon.setPositionByController(this.posX, this.posY, this.posZ);
+                }
+                else
+                {
+                    sophon.setTrackingPlayer(this.trackingPlayer);
                 }
             }
             catch (NullPointerException e)
